fbabf6727007a4458dab6c636e326d0e41a98530
Geetha552 <vutukurigeethanl@gmail.com>
2026-01-03 20:30:48 +0530

    created ResturantCard

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import {Navigate} from "react-router-dom";
+import { children, useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+ const ProtectedRoute = ({ children, role }) => {
+    const {user} = useContext(AuthContext);
+
+    if(!user) return <Navigate to="/" />;
+    if(role && user.role !==role) return <Navigate to="/" />;
+    return children;
+ };
+ export default ProtectedRoute;
